bece301f45d089d2aa08a57dd0bf7b971e4637dd
Sergey New <rexarko@mail.ru>
2018-05-14 10:44:04 +0500

    правки по корзине

--- app/js-es6/common.js
+++ app/js-es6/common.js
@@ -1,4 +1,4 @@
-Vue.use(Vuex);
+
 // sergey new code-------------
 $(document).ready(function(){
 	// слайдер
@@ -191,118 +191,118 @@ const loadScripts = e => {
 };
 
 
-Vue.component("cart-line", {
-	props: {
+// Vue.component("cart-line", {
+// 	props: {
 
-	},
-	data: () => ({
+// 	},
+// 	data: () => ({
 
-	}),
-	mounted(){
-		console.log("cart-line mounted");
-	},
-	methods: {
-		deleteFromCart(id){
-			Store.commit("deleteGoods", id);
-		},
-	},
-	computed: {
-		goodsList: () => Store.state.goodsInCart,
-		goodsCount: () => Store.state.goodsInCart.length,
-		goodsPrice(){
-			let totalSumm = 0;
+// 	}),
+// 	mounted(){
+// 		console.log("cart-line mounted");
+// 	},
+// 	methods: {
+// 		deleteFromCart(id){
+// 			Store.commit("deleteGoods", id);
+// 		},
+// 	},
+// 	computed: {
+// 		goodsList: () => Store.state.goodsInCart,
+// 		goodsCount: () => Store.state.goodsInCart.length,
+// 		goodsPrice(){
+// 			let totalSumm = 0;
 
-			for (let i in this.goodsList)
-				totalSumm += this.goodsList[i].price * this.goodsList[i].count
+// 			for (let i in this.goodsList)
+// 				totalSumm += this.goodsList[i].price * this.goodsList[i].count
 
-			return totalSumm;
-		}
-	}
-});
+// 			return totalSumm;
+// 		}
+// 	}
+// });
 
-window.Store = new Vuex.Store({
-	state: {
-		goodsInCart: []
-	},
-	mutations: {
-		addGoods(state, goods = []){
-			for (let i in goods){
-				state.goodsInCart.push(goods[i]);
-			}
-		},
-		deleteGoods(state, goodsId){
-			let key, canDelete = false;
+// window.Store = new Vuex.Store({
+// 	state: {
+// 		goodsInCart: []
+// 	},
+// 	mutations: {
+// 		addGoods(state, goods = []){
+// 			for (let i in goods){
+// 				state.goodsInCart.push(goods[i]);
+// 			}
+// 		},
+// 		deleteGoods(state, goodsId){
+// 			let key, canDelete = false;
 
-			for (let i in state.goodsInCart){
-				key = i;
+// 			for (let i in state.goodsInCart){
+// 				key = i;
 
-				if (state.goodsInCart[i].id == goodsId){
-					canDelete = true;
-					break;
-				}
-			}
+// 				if (state.goodsInCart[i].id == goodsId){
+// 					canDelete = true;
+// 					break;
+// 				}
+// 			}
 
 
-			if (canDelete)
-				state.goodsInCart.splice(key, 1);
-			else
-				console.log("Такого товара нет в списке");
-		}
-	}
-});
+// 			if (canDelete)
+// 				state.goodsInCart.splice(key, 1);
+// 			else
+// 				console.log("Такого товара нет в списке");
+// 		}
+// 	}
+// });
 
-window.App = new Vue({
-	el: "#page-wr",
-	store: Store,
-	data: {
+// window.App = new Vue({
+// 	el: "#page-wr",
+// 	store: Store,
+// 	data: {
 
-	},
-	mounted(){
-		loadScripts();
-		console.log("app mounted");
-	}
-});
+// 	},
+// 	mounted(){
+// 		loadScripts();
+// 		console.log("app mounted");
+// 	}
+// });
 
-$(e => {
-	Store.commit("addGoods", [
-		{
-			id: 0,
-			name: "Гель-лак Rock Nail Baby Nude",
-			info: "045 Blue Ocean",
-			imgSrc: "img/photos/nail.png",
-			link: "#",
-			price: 250,
-			count: 1,
-		},
-		{
-			id: 1,
-			name: "Гель-лак Rock Nail Baby Nude",
-			info: "045 Blue Ocean",
-			imgSrc: "img/photos/nail.png",
-			link: "#",
-			price: 250,
-			count: 3,
-		},
-		{
-			id: 2,
-			name: "Гель-лак Rock Nail Baby Nude",
-			info: "045 Blue Ocean",
-			imgSrc: "img/photos/nail.png",
-			link: "#",
-			price: 250,
-			count: 2,
-		},
-		{
-			id: 3,
-			name: "Гель-лак Rock Nail Baby Nude",
-			info: "045 Blue Ocean",
-			imgSrc: "img/photos/nail.png",
-			link: "#",
-			price: 250,
-			count: 1,
-		},
-	]);
-})
+// $(e => {
+// 	Store.commit("addGoods", [
+// 		{
+// 			id: 0,
+// 			name: "Гель-лак Rock Nail Baby Nude",
+// 			info: "045 Blue Ocean",
+// 			imgSrc: "img/photos/nail.png",
+// 			link: "#",
+// 			price: 250,
+// 			count: 1,
+// 		},
+// 		{
+// 			id: 1,
+// 			name: "Гель-лак Rock Nail Baby Nude",
+// 			info: "045 Blue Ocean",
+// 			imgSrc: "img/photos/nail.png",
+// 			link: "#",
+// 			price: 250,
+// 			count: 3,
+// 		},
+// 		{
+// 			id: 2,
+// 			name: "Гель-лак Rock Nail Baby Nude",
+// 			info: "045 Blue Ocean",
+// 			imgSrc: "img/photos/nail.png",
+// 			link: "#",
+// 			price: 250,
+// 			count: 2,
+// 		},
+// 		{
+// 			id: 3,
+// 			name: "Гель-лак Rock Nail Baby Nude",
+// 			info: "045 Blue Ocean",
+// 			imgSrc: "img/photos/nail.png",
+// 			link: "#",
+// 			price: 250,
+// 			count: 1,
+// 		},
+// 	]);
+// })
 
 $(function() {
 	if($(window).width() > 790) {
